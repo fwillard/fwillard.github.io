70a41793bd7dc78c1dbff8b34825fbecaa056d3a
Finn Willard <finn.willard@gmail.com>
2024-09-11 19:35:15 -0700

    improvements

--- count.js
+++ count.js
@@ -1,9 +1,11 @@
+
 setInterval(function(){
-    let eos = new Date('January 1, 2024 00:00:00');
-    let today = new Date
-    let diff = today - eos
+    var eos = new Date('January 1, 2024 00:00:00');
+    var today = new Date
+    var diff = today - eos
     
     document.getElementById('count').innerText = dhms(diff)
+    document.getElementById('text').innerText = "that Alex has not done his punishment."
     }, 1000)
     
     function dhms(ms){
@@ -14,5 +16,8 @@ setInterval(function(){
       const minutes = Math.floor(hoursms / (60*1000));
       const minutesms = ms % (60*1000);
       const sec = Math.floor(minutesms / 1000);
-      return days + " Days, " + hours + " Hours, " + minutes + " Minutes, " + sec + " Seconds.";
+      return days + " Days, " + hours + " Hours, " + minutes + " Minutes, and " + sec + " Seconds";
     }
+$(window).bind("load", function() {
+    $("body").fadeIn(100);
+    });
